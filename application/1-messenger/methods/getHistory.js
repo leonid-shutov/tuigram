86944917e483c679fba7adae67221ed22dd2faf8
leonid-shutov <leonid.shutov.main@gmail.com>
2025-12-28 23:15:19 +0100

    chat state draft

--- application/1-messenger/methods/getHistory.js
+++ application/1-messenger/methods/getHistory.js
@@ -1,11 +1,13 @@
 //(() =>
 //async function* (chatId, pageSize) {
-//const cachedMessages = module.cache.chats.get(chatId) ?? [];
-//cachedMessages.forEach(yield);
+//const cachedMessages = module.cache.chats.get(chatId) ?? [[]];
+//yield cachedMessages;
 
 //let offset = cachedMessages.at(-1)?.next ?? undefined;
 //while (true) {
 //const params = { offset, limit: pageSize };
+
+// the first message in the array is the most recent message in the chat
 //const messages = await messenger.tg.getHistory(chatId, params);
 
 //cachedMessages.push(messages);
--- application/4-sections/3-chat/state/state.js
+++ application/4-sections/3-chat/state/state.js
@@ -1,4 +1,13 @@
 ({
   chatId: undefined,
   iterator: null,
+  messages: [],
+  selected: null,
+  //window: { start: 0, end: module.ui.getMaxLines() - 1 },
+
+  async open(chatId, iterator) {
+    self.chatId = chatId;
+    const { value: messages } = await iterator.next();
+    self.messages = messages;
+  },
 });
