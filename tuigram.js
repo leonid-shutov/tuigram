4419395cbbd832fc4d3d68b0c4e207e7cc33c4f3
leonid-shutov <leonid.shutov@hey.com>
2026-05-24 15:48:21 +0200

    log

--- tuigram.js
+++ tuigram.js
@@ -2,6 +2,23 @@
 
 const uncommonjs = require('@leonid-shutov/uncommonjs');
 const blessed = require('blessed');
+const { writeFileSync, appendFileSync } = require('fs');
+
+const LOG_FILE = 'app.log';
+writeFileSync(LOG_FILE, ''); // truncate on startup
+
+function formatArgs(args) {
+  return args.map((arg) => (typeof arg === 'object' ? JSON.stringify(arg, null, 2) : String(arg))).join(' ');
+}
+
+function writeLog(...args) {
+  const line = `${formatArgs(args)}\n`;
+  appendFileSync(LOG_FILE, line);
+}
+
+const mockConsole = {
+  log: (...args) => writeLog(...args),
+};
 
 process.on('unhandledRejection', (reason, promise) => {
   console.dir({ promise });
@@ -14,7 +31,7 @@ process.on('uncaughtException', (...args) => {
 
 (async () => {
   await uncommonjs.loadApplication({
-    console,
+    console: mockConsole,
     blessed,
     process,
   });
