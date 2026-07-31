57883019587dfb659efb35d317453224ecbd4c3c
leonid-shutov <leonid.shutov@hey.com>
2026-07-31 23:21:50 +0200

    fmt

--- src/3-ui/4-layout/6-loadDialogs.js
+++ src/3-ui/4-layout/6-loadDialogs.js
@@ -1,5 +1,3 @@
 const chunkSize = 10;
-Array.fromAsync(AsyncIterator.take(messenger.iterDialogs(chunkSize), chunkSize)).then(
-  ui.sections.dialogs.setDialogs,
-);
+Array.fromAsync(AsyncIterator.take(messenger.iterDialogs(chunkSize), chunkSize)).then(ui.sections.dialogs.setDialogs);
 void Array.fromAsync(messenger.iterDialogs()).then(ui.sections.dialogs.setDialogs);
