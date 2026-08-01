062da459acf5aec8ab41c02f33b9ca9540bc1a7a
leonid-shutov <leonid.shutov@hey.com>
2026-08-01 21:37:31 +0200

    WIP

--- src/2-nvim/2-events.js
+++ src/2-nvim/2-events.js
@@ -1,25 +1,35 @@
-const handlers = {
-  cursor_goto: async () => {
-    const [y, x] = await nvim.client.window.cursor;
-    nvim.emit('cursor', { x, y });
-  },
-  put: Rate.debounce(async () => {
-    const buffer = await nvim.client.buffer;
-    const lines = await buffer.lines;
-    const [y, x] = await nvim.client.window.cursor;
-    nvim.emit('lines', lines, { x, y });
-  }),
-  mode_change: ([[mode]]) => {
-    nvim.mode = mode;
-    nvim.emit('mode', mode);
-  },
+const MODE = {
+  n: 'normal',
+  i: 'insert',
+  v: 'visual',
+  V: 'visual',
+  R: 'replace',
+  c: 'command',
+  t: 'terminal',
+  s: 'select',
 };
 
-nvim.client.on('notification', (method, args) => {
-  if (method !== 'redraw') return;
+const readCursor = async () => {
+  const [y, x] = await nvim.client.window.cursor;
+  return { x, y };
+};
 
-  for (const [event, ...updates] of args) {
-    const handler = handlers[event];
-    if (handler !== undefined) handler(updates);
+nvim.client.channelId.then((chan) => {
+  nvim.client.command(`autocmd TextChanged,TextChangedI * call rpcnotify(${chan}, 'lines')`);
+  nvim.client.command(`autocmd CursorMoved,CursorMovedI * call rpcnotify(${chan}, 'cursor')`);
+  nvim.client.command(`autocmd ModeChanged * call rpcnotify(${chan}, 'mode', mode())`);
+});
+
+nvim.client.on('notification', async (method, args) => {
+  if (method === 'lines') {
+    const buffer = await nvim.client.buffer;
+    const lines = await buffer.lines;
+    nvim.emit('lines', lines, await readCursor());
+  } else if (method === 'cursor') {
+    nvim.emit('cursor', await readCursor());
+  } else if (method === 'mode') {
+    const mode = MODE[args[0]?.[0]] ?? args[0];
+    nvim.mode = mode;
+    nvim.emit('mode', mode);
   }
 });
