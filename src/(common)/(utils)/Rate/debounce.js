8a98b1913a4b588180cdf5b7335710f34692044c
leonid-shutov <leonid.shutov@hey.com>
2026-08-01 21:37:31 +0200

    refactor nvim

--- src/(common)/(utils)/Rate/debounce.js
+++
@@ -1,7 +0,0 @@
-(fn, delay = 0) => {
-  let timer = null;
-  return (...args) => {
-    node.timers.clearTimeout(timer);
-    timer = node.timers.setTimeout(() => fn(...args), delay);
-  };
-};
--- src/2-nvim/1-client.js
+++ src/2-nvim/1-client.js
@@ -1,6 +1,16 @@
 const { findNvim, attach } = npm.neovim;
-const found = findNvim({ orderBy: 'desc', minVersion: '0.9.0' });
-const path = found.matches[0].path;
-const nvimProc = node.child_process.spawn(path, ['--embed']);
-const client = attach({ proc: nvimProc, logger: console });
-client.uiAttach(5, 5, {}).then(() => client);
+const path = findNvim({ orderBy: 'desc', minVersion: '0.9.0' }).matches[0].path;
+
+const proc = node.child_process.spawn(path, ['-u', 'NONE', '-i', 'NONE', '-n', '--embed']);
+proc.on('error', (err) => console.error('nvim failed to start:', err));
+
+const client = attach({ proc });
+client
+  .uiAttach(80, 24, {
+    rgb: false,
+    ext_cmdline: false,
+    ext_popupmenu: false,
+    ext_tabline: false,
+    ext_wildmenu: false,
+  })
+  .then(() => client);
