bac2ca46d9c5eab02728fd03353dc92a8987b459
leonid-shutov <leonid.shutov@hey.com>
2026-09-02 22:10:11 +0200

    select messages using arrows

--- src/5-ui/2-sections/2-chat/(public)/key.js
+++ src/5-ui/2-sections/2-chat/(public)/key.js
@@ -1,6 +1,6 @@
 /** @type {ChatSelf['key']} */
 ({ name }) => {
   /** @type {Record<string, (() => unknown) | undefined>} */
-  const bindings = { k: self.up, j: self.down };
+  const bindings = { k: self.up, up: self.up, j: self.down, down: self.down };
   bindings[name]?.();
 };
