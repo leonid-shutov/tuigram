1600e85814ffe2cf11574e01e99bd90643be3872
leonid-shutov <leonid.shutov@hey.com>
2026-09-03 23:15:04 +0200

    open picker on slash

--- src/5-ui/3-layout/1-layout.js
+++ src/5-ui/3-layout/1-layout.js
@@ -8,4 +8,7 @@
     3: { label: ' 3 ', section: 'messagePrompt' },
     [Keys.CTRL_P]: { label: ' Jump to chat ', section: 'picker' },
   },
+  sectionShortcuts: {
+    dialogs: { [Keys.SLASH]: 'picker' },
+  },
 });
