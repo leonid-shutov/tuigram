393e90ead54faf4c766338605ab092140f38384a
leonid-shutov <leonid.shutov@hey.com>
2026-09-03 22:26:54 +0200

    fix pending message id

--- src/(common)/(utils)/Random/id.js
+++ src/(common)/(utils)/Random/id.js
@@ -1,4 +1,4 @@
 // Random integer for temporary client-side ids (e.g. a message that hasn't been
 // confirmed by the server yet). Large enough that collisions are implausible.
 /** @type {typeof Random.id} */
-() => node.crypto.randomInt(2 ** 48);
+() => node.crypto.randomInt(2 ** 48 - 1);
